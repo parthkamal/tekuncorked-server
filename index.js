fad21a926d4d456e8a0fdb9d74ae9d2c45d9417b
parthkamal <kamalparth40@gmail.com>
2023-09-23 00:16:26 +0530

    added environment variable

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express'); 
 
 const app = express();
-const PORT = 3000; 
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+const PORT = process.env.PORT;
+
+
 
 app.get('/', (req, res) => {
 	res.send('hello world')
